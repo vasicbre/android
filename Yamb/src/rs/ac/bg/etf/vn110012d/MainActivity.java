2971ac2903cf7658e4924ca417006a0fe2a8241a
Nemanja Vasic <vasicnem@gmail.com>
2015-03-04 19:35:22 +0100

    Created gameplay layout

--- Yamb/src/rs/ac/bg/etf/vn110012d/MainActivity.java
+++ Yamb/src/rs/ac/bg/etf/vn110012d/MainActivity.java
@@ -1,17 +1,37 @@
 package rs.ac.bg.etf.vn110012d;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+	
+	Button newgame, settings, history, resume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        newgame = (Button) findViewById(R.id.new_game_button);
+        settings = (Button) findViewById(R.id.settings_button);
+        history = (Button) findViewById(R.id.history_button);
+        resume = (Button) findViewById(R.id.resume_button);        
+        resume.setVisibility(View.GONE);
+        
+        newgame.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), GameplayActivity.class);
+				startActivity(intent);				
+			}
+		});
     }
 
 
