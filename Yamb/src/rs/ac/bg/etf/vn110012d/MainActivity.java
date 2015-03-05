a1919943ba19101f4fb48ca784a7e0b994851a5f
Nemanja Vasic <vasicnem@gmail.com>
2015-03-05 09:51:22 +0100

    added dialog for selecting number of players

--- Yamb/src/rs/ac/bg/etf/vn110012d/MainActivity.java
+++ Yamb/src/rs/ac/bg/etf/vn110012d/MainActivity.java
@@ -1,56 +1,92 @@
 package rs.ac.bg.etf.vn110012d;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 
+public class MainActivity extends Activity implements
+		NumberPicker.OnValueChangeListener {
 
-public class MainActivity extends Activity {
-	
 	Button newgame, settings, history, resume;
+	static Dialog dialog;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        newgame = (Button) findViewById(R.id.new_game_button);
-        settings = (Button) findViewById(R.id.settings_button);
-        history = (Button) findViewById(R.id.history_button);
-        resume = (Button) findViewById(R.id.resume_button);        
-        resume.setVisibility(View.GONE);
-        
-        newgame.setOnClickListener(new View.OnClickListener() {
-			
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		newgame = (Button) findViewById(R.id.new_game_button);
+		settings = (Button) findViewById(R.id.settings_button);
+		history = (Button) findViewById(R.id.history_button);
+		resume = (Button) findViewById(R.id.resume_button);
+		resume.setVisibility(View.GONE);
+
+		newgame.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(), GameplayActivity.class);
-				startActivity(intent);				
+				showDialog();
 			}
 		});
-    }
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public void showDialog() {
+		final Dialog d = new Dialog(MainActivity.this);
+		d.setTitle("Number of players");
+		d.setContentView(R.layout.dialog);
+		Button play = (Button) d.findViewById(R.id.play_button);
+		Button cancel = (Button) d.findViewById(R.id.cancel_button);
+		final NumberPicker np = (NumberPicker) d
+				.findViewById(R.id.number_picker);
+		np.setMaxValue(100);
+		np.setMinValue(0);
+		np.setWrapSelectorWheel(false);
+		np.setOnValueChangedListener(this);
+		play.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(),	GameplayActivity.class);
+				startActivity(intent);
+			}
+		});
+		cancel.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				d.dismiss();
+			}
+		});
+		d.show();
+
+	}
+
+	@Override
+	public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+		// TODO Auto-generated method stub
+
+	}
 }
