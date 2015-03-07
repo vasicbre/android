07a46e80e5a9ddd7b03c28f9aeef33fc55aa5761
Nemanja Vasic <vasicnem@gmail.com>
2015-03-07 20:23:41 +0100

    added dice rolling using shake movement

--- Yamb/src/rs/ac/bg/etf/vn110012d/GameplayActivity.java
+++ Yamb/src/rs/ac/bg/etf/vn110012d/GameplayActivity.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,10 +23,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class GameplayActivity extends Activity {
+public class GameplayActivity extends Activity implements SensorEventListener {
 
 	int[] diceValues;
 	boolean[] selectedDice;
+
+	private SensorManager senSensorManager;
+	private Sensor senAccelerometer;
+	private long lastUpdate = 0;
+	private float last_x, last_y, last_z;
+	private static final int SHAKE_THRESHOLD = 600;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +45,33 @@ public class GameplayActivity extends Activity {
 		populateScoreCells(R.id.min_max_sum);
 		populateScoreCells(R.id.spec_sum);
 		loadDice();
+
+		senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		senAccelerometer = senSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		senSensorManager.registerListener(this, senAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		senSensorManager.unregisterListener(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		senSensorManager.registerListener(this, senAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
 	private void loadDice() {
 		ImageView[] dice = new ImageView[6];
 		diceValues = new int[6];
 		selectedDice = new boolean[6];
+
 		for (int i = 0; i < 6; i++) {
 			diceValues[i] = i + 1;
 			selectedDice[i] = false;
@@ -301,5 +332,57 @@ public class GameplayActivity extends Activity {
 
 			return convertView;
 		}
+	}
+	
+	void rollDice() {
+		for(int i = 0; i < 6; i++) {
+			if(!selectedDice[i]) {
+				diceValues[i] = (int) (Math.random() * 6) + 1;
+				ImageView iv = (ImageView) findViewById(diceSlotId(i));
+				iv.setImageResource(diceId(diceValues[i] - 1, selectedDice[i]));
+			}
+		}
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent sensorEvent) {
+		Sensor mySensor = sensorEvent.sensor;
+
+		if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			float x = sensorEvent.values[0];
+			float y = sensorEvent.values[1];
+			float z = sensorEvent.values[2];
+
+			// check whether more than 100 milliseconds have passed since the
+			// last time onSensorChanged was invoked
+			long curTime = System.currentTimeMillis();
+
+			if ((curTime - lastUpdate) > 100) {
+				long diffTime = (curTime - lastUpdate);
+				lastUpdate = curTime;
+
+				float speed = Math.abs(x + y + z - last_x - last_y - last_z)
+						/ diffTime * 10000;
+
+				if (speed > SHAKE_THRESHOLD) {
+					Toast toast = Toast.makeText(getApplicationContext(),
+							"Shake Shake Shake !!!", Toast.LENGTH_SHORT);
+					toast.show();
+					
+					rollDice();
+				}
+
+				last_x = x;
+				last_y = y;
+				last_z = z;
+			}
+		}
+
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
 	}
 }
