443a92d003781f367ace3eeadaf0946be87ca3a9
Nemanja Vasic <vasicnem@gmail.com>
2015-04-08 19:20:36 +0200

    created database to store all moves to be able to do replay of a game

--- Yamb/src/rs/ac/bg/etf/vn110012d/DataAccessHandler.java
+++ Yamb/src/rs/ac/bg/etf/vn110012d/DataAccessHandler.java
@@ -1,6 +1,9 @@
 package rs.ac.bg.etf.vn110012d;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -12,9 +15,9 @@ public class DataAccessHandler {
 
 	private SQLiteDatabase database;
 	private MySQLiteHelper dbHelper;
-	
-	private String[] allColumns = { MySQLiteHelper.ID,
-		      MySQLiteHelper.NAME, MySQLiteHelper.SCORE };
+
+	private String[] allColumns = { MySQLiteHelper.ID, MySQLiteHelper.NAME,
+			MySQLiteHelper.SCORE };
 
 	public DataAccessHandler(Context context) {
 		dbHelper = new MySQLiteHelper(context);
@@ -28,22 +31,81 @@ public class DataAccessHandler {
 		dbHelper.close();
 	}
 
-	public long addScore(String name, int score) {
+	public long addScore(int score, long gameId, long playerId) {
 		ContentValues values = new ContentValues();
-		values.put(MySQLiteHelper.NAME, name);
+		values.put(MySQLiteHelper.GAME_ID, gameId);
+		values.put(MySQLiteHelper.PLAYER_ID, playerId);
 		values.put(MySQLiteHelper.SCORE, score);
 		return database.insert(MySQLiteHelper.SCORE_TABLE, null, values);
 	}
 
+	public long addGame() {
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteHelper.START, getDateTime());
+		values.put(MySQLiteHelper.END, getDateTime());
+		return database.insert(MySQLiteHelper.GAME_TABLE, null, values);
+	}
+
+	public long addPlayer(String name) {
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteHelper.NAME, name);
+		return database.insert(MySQLiteHelper.PLAYER_TABLE, null, values);
+	}
+
+	public long addMove(int ord, int x, int y, int value, long gameId,
+			long playerId) {
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteHelper.BOARD_X, 0);
+		values.put(MySQLiteHelper.BOARD_Y, 0);
+		values.put(MySQLiteHelper.VALUE, 0);
+		values.put(MySQLiteHelper.GAME_ID, gameId);
+		values.put(MySQLiteHelper.PLAYER_ID, playerId);
+		values.put(MySQLiteHelper.ORD, ord);
+		return database.insert(MySQLiteHelper.MOVE_TABLE, null, values);
+	}
+	
+	public long addRoll(int ord, String result, String locked, int moveId) {
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteHelper.ORD, ord);
+		values.put(MySQLiteHelper.MOVE_ID, moveId);
+		values.put(MySQLiteHelper.RESULT, result);
+		values.put(MySQLiteHelper.LOCKED, locked);
+		return database.insert(MySQLiteHelper.ROLL_TABLE, null, values);
+	}
+	
+	public void updateGame(long gameId) {
+		String strFilter = MySQLiteHelper.ID + "=" + gameId;
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteHelper.END, getDateTime());
+		database.update(MySQLiteHelper.GAME_TABLE, values, strFilter, null);
+	}
+	
+	public void updateMove(long moveId, int x, int y, int value) {
+		String strFilter = MySQLiteHelper.ID + "=" + moveId;
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteHelper.BOARD_X, x);
+		values.put(MySQLiteHelper.BOARD_Y, y);
+		values.put(MySQLiteHelper.VALUE, value);
+		database.update(MySQLiteHelper.MOVE_TABLE, values, strFilter, null);
+	}
+	
 	public void getAllScores(List<String> names, List<Integer> scores) {
-		Cursor cursor = database.query(MySQLiteHelper.SCORE_TABLE, allColumns, null, null, null, null, null);
+		Cursor cursor = database.query(MySQLiteHelper.SCORE_TABLE, allColumns,
+				null, null, null, null, null);
 		cursor.moveToFirst();
-		while(!cursor.isAfterLast()) {
+		while (!cursor.isAfterLast()) {
 			names.add(cursor.getString(1));
 			scores.add(cursor.getInt(2));
 			cursor.moveToNext();
 		}
-		
+
 		cursor.close();
+	}
+
+	private String getDateTime() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(
+				"yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+		Date date = new Date();
+		return dateFormat.format(date);
 	}
 }
--- Yamb/src/rs/ac/bg/etf/vn110012d/MySQLiteHelper.java
+++ Yamb/src/rs/ac/bg/etf/vn110012d/MySQLiteHelper.java
@@ -6,17 +6,66 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
-	public static final String SCORE_TABLE = "scores";
-	public static final String ID = "_id";
-	public static final String NAME = "name";
-	public static final String SCORE = "score";
-
 	private static final String DATABASE = "scores.db";
 	private static final int DATABASE_VERSION = 1;
 
-	private static final String DATABASE_CREATE = "create table " + SCORE_TABLE
-			+ "(" + ID + " integer primary key autoincrement" + "," + NAME
-			+ " text not null" + "," + SCORE + " integer not null" + ");";
+	// Table names
+	public static final String SCORE_TABLE = "scores";
+	public static final String GAME_TABLE = "games";
+	public static final String MOVE_TABLE = "moves";
+	public static final String PLAYER_TABLE = "players";
+	public static final String ROLL_TABLE = "rolls";
+
+	// Common column name
+	public static final String ID = "_id";
+	public static final String GAME_ID = "game_id";
+	public static final String PLAYER_ID = "player_id";
+
+	// SCORES table columns
+	public static final String SCORE = "score";
+
+	// GAMES table columns
+	public static final String START = "start";
+	public static final String END = "end";
+
+	// PLAYERS table columns
+	public static final String NAME = "name";
+
+	// MOVES table columns
+	public static final String ORD = "ord";
+	public static final String BOARD_X = "board_x";
+	public static final String BOARD_Y = "board_y";
+	public static final String VALUE = "value";
+
+	// ROLLS table columns
+	public static final String RESULT = "result";
+	public static final String LOCKED = "locked";
+	public static final String MOVE_ID = "move_id";
+
+	private static final String CREATE_TABLE_GAME = "create table "
+			+ GAME_TABLE + "(" + ID + " integer primary key autoincrement"
+			+ "," + START + " datetime," + END + " datetime);";
+
+	private static final String CREATE_TABLE_PLAEYR = "create table "
+			+ PLAYER_TABLE + "(" + ID + " integer primary key autoincrement"
+			+ "," + NAME + " text not null" + ");";
+
+	private static final String CREATE_TABLE_SCORE = "create table "
+			+ SCORE_TABLE + "(" + ID + " integer primary key autoincrement"
+			+ "," + PLAYER_ID + " integer not null," + GAME_ID
+			+ " integer not null," + SCORE + " integer not null);";
+
+	private static final String CREATE_TABLE_MOVE = "create table "
+			+ MOVE_TABLE + "(" + ID + " integer primary key autoincrement"
+			+ ORD + " integer not null," + BOARD_X + " integer not null, "
+			+ VALUE + " integer not null," + BOARD_Y + " integer not null,"
+			+ GAME_ID + " integer not null," + PLAYER_ID
+			+ " integer not null);";
+
+	private static final String CREATE_TABLE_ROLL = "create table "
+			+ ROLL_TABLE + "(" + ID + " integer primary key autoincrement"
+			+ ORD + " integet not null," + RESULT + " text not null," + LOCKED
+			+ " text not null" + MOVE_ID + " integer not null);";
 
 	public MySQLiteHelper(Context context) {
 		super(context, DATABASE, null, DATABASE_VERSION);
@@ -24,7 +73,11 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL(DATABASE_CREATE);
+		db.execSQL(CREATE_TABLE_GAME);
+		db.execSQL(CREATE_TABLE_PLAEYR);
+		db.execSQL(CREATE_TABLE_SCORE);
+		db.execSQL(CREATE_TABLE_MOVE);
+		db.execSQL(CREATE_TABLE_ROLL);
 	}
 
 	@Override
